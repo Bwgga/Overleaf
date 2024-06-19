8fd16e6e47575571924435558d74ce59747b0ccd
Bwgga <bwgga.gi@gmail.com>
2024-06-19 16:22:17 -0400

    play button code

--- scripts/main_page.gd
+++ scripts/main_page.gd
@@ -2,8 +2,12 @@ extends Node2D
 
 
 # Play button
-
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/levels_page.tscn")
 
 # Quit button
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+
