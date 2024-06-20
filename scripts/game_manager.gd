3361905f8e32dc044fdcf15e8f2a756b74cdd944
Bwgga <bwgga.gi@gmail.com>
2024-06-20 15:52:42 -0400

    made better stats label

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,8 +2,8 @@ extends Node
 
 var coins = 0
 
-@onready var stats = $"../Player/Camera2D/Stats"
+@onready var stats = $"../UI/Stats"
 
 func add_coin():
 	coins += 1
-	stats.text = "Coins: " + str(coins)
+	stats.text = "Coins: " + str(coins) + "\n"
