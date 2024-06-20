f5521f7c4d8c8d8070321cf076666b62592cdc96
Bwgga <bwgga.gi@gmail.com>
2024-06-20 14:50:42 -0400

    added player anim

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,19 +7,47 @@ const JUMP_VELOCITY = -300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var animated_sprite = $AnimatedSprite2D
+
+var last_dir : int
 
 func _physics_process(delta):
-	# Add the gravity.
+	# Add the gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	# Get the direction from input (-1, 0, 1)
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	# play animations
+	if not is_on_floor():
+			animated_sprite.play("jump")
+	else:
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	
+	
+	# check for which direction and flip animations
+	if direction > 0:
+		animated_sprite.flip_h = false
+		last_dir = 1
+	elif direction < 0: 
+		animated_sprite.flip_h = true
+		last_dir = -1
+	else:
+		if last_dir > 0:
+			animated_sprite.flip_h = false
+		elif last_dir < 0:
+			animated_sprite.flip_h = true
+		else:
+			animated_sprite.flip_h = false
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
