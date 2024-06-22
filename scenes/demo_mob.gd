97d8accbab5aa450b4dbbd0d10a01e4efb4cad24
Lou01 <112272348+Lou01-proxima@users.noreply.github.com>
2024-06-22 13:31:43 -0400

    animated mob

--- scenes/demo_mob.gd
+++ scenes/demo_mob.gd
@@ -1,1 +1,18 @@
-extends AnimatedSprite2D
+extends Node2D
+
+@onready var ray_cast_right = $RayCastRight
+@onready var ray_cast_left = $RayCastLeft
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+const SPEED = 30
+var direction = 1
+
+func _process(delta):
+	if ray_cast_right.is_colliding():
+		direction = -1
+		animated_sprite_2d.flip_h = true
+	elif ray_cast_left.is_colliding():
+		direction = 1
+		animated_sprite_2d.flip_h = false
+	
+	position.x += SPEED * delta * direction
