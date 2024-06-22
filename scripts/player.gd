85eec1012ce2bd2949f6395e35962e4009d1ceb8
Lou01 <112272348+Lou01-proxima@users.noreply.github.com>
2024-06-22 11:37:30 -0400

    blocked input when killzone entered

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 
@@ -11,46 +10,54 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var last_dir : int
 
+var has_died : bool = false
+
 func _physics_process(delta):
 	# Add the gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if not has_died:
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	# Get the direction from input (-1, 0, 1)
-	var direction = Input.get_axis("move_left", "move_right")
-	
-	# play animations
-	if not is_on_floor():
-			animated_sprite.play("jump")
-	else:
-		if direction == 0:
-			animated_sprite.play("idle")
+		# Get the direction from input (-1, 0, 1)
+		var direction = Input.get_axis("move_left", "move_right")
+		
+		# play animations
+		if not is_on_floor():
+				animated_sprite.play("jump")
 		else:
-			animated_sprite.play("run")
-	
-	
-	# check for which direction and flip animations
-	if direction > 0:
-		animated_sprite.flip_h = false
-		last_dir = 1
-	elif direction < 0: 
-		animated_sprite.flip_h = true
-		last_dir = -1
-	else:
-		if last_dir > 0:
+			if direction == 0:
+				animated_sprite.play("idle")
+			else:
+				animated_sprite.play("run")
+		
+		
+		# check for which direction and flip animations
+		if direction > 0:
 			animated_sprite.flip_h = false
-		elif last_dir < 0:
+			last_dir = 1
+		elif direction < 0: 
 			animated_sprite.flip_h = true
+			last_dir = -1
 		else:
-			animated_sprite.flip_h = false
-	
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+			if last_dir > 0:
+				animated_sprite.flip_h = false
+			elif last_dir < 0:
+				animated_sprite.flip_h = true
+			else:
+				animated_sprite.flip_h = false
+		
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+		move_and_slide()
+
+
+func _on_killzone_body_entered(body):
+	has_died = true
+	# add death animation (?)
