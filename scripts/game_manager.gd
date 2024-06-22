b33dee9cb6bc79389858c2ce68259eb344bccd53
ana <anasofitorresbe@gmail.com>
2024-06-22 15:47:09 -0400

    health bar

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,9 +1,16 @@
 extends Node
 
 var coins = 0
-
 @onready var stats = $"../UI/Stats"
 
 func add_coin():
 	coins += 1
 	stats.text = " \nCoins: " + str(coins) + "\n"
+
+
+@onready var health_bar = %HealthBar
+@onready var health_coin = $"../HeathCoin/Health Coin"
+
+func _add_health_coin(body):
+	health_bar.add_healthcoin()
+	health_coin.dissapear()
