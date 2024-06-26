1474cd39850fbdfdf10d3551d12f05476543cc0c
Lou01 <112272348+Lou01-proxima@users.noreply.github.com>
2024-06-26 15:23:14 -0400

    killzone deals max damage

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,10 +1,12 @@
 extends Area2D
 
 @onready var timer = $Timer
+@onready var health_bar = %HealthBar
 
 func _on_body_entered(body):
 	# print("Killzone detects " + str(body))
 	timer.start()
+	health_bar.get_hurt(200)
 	body.get_node("AnimatedSprite2D").play("death")
 
 func _on_timer_timeout():
