f6d9b822dbc5c9e2620a87767cae2ee175f211aa
Lou01 <112272348+Lou01-proxima@users.noreply.github.com>
2024-06-26 15:56:50 -0400

    simplified death; animation and blocked controls actually work

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -4,9 +4,8 @@ extends Area2D
 @onready var health_bar = %HealthBar
 
 func _on_body_entered(body):
-	# print("Killzone detects " + str(body))
-	timer.start()
 	health_bar.get_hurt(200)
+	timer.start()
 	body.get_node("AnimatedSprite2D").play("death")
 
 func _on_timer_timeout():
