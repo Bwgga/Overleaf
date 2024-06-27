a8d58cc2ba5a64698d7fadb79648334148f90d45
Lou01 <112272348+Lou01-proxima@users.noreply.github.com>
2024-06-26 22:13:22 -0400

    added attack action and animation
    the 5A update

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,14 +2,20 @@ extends CharacterBody2D
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
+const ATTACK_BOOST = 2.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var health_bar = %HealthBar
+@onready var attack_timer = $AttackTimer
+@onready var attack_cooldown_timer = $AttackCooldownTimer
 
 var last_dir : int
+
+var is_attacking : bool = false
+var can_attack  : bool = true
 
 func _physics_process(delta):
 	# Add the gravity
@@ -23,16 +29,6 @@ func _physics_process(delta):
 
 		# Get the direction from input (-1, 0, 1)
 		var direction = Input.get_axis("move_left", "move_right")
-		
-		# play animations
-		if not is_on_floor():
-				animated_sprite.play("jump")
-		else:
-			if direction == 0:
-				animated_sprite.play("idle")
-			else:
-				animated_sprite.play("run")
-		
 		
 		# check for which direction and flip animations
 		if direction > 0:
@@ -49,10 +45,41 @@ func _physics_process(delta):
 			else:
 				animated_sprite.flip_h = false
 		
+		#sword attack
+		if Input.is_action_just_pressed("attack") and can_attack:
+			is_attacking = true
+			can_attack = false
+			#play an attack animation
+			attack_timer.start()
+		
+		# play animations
+		if is_attacking:
+			animated_sprite.play("attack")
+		elif not is_on_floor():
+				animated_sprite.play("jump")
+		else:
+			if direction == 0:
+				animated_sprite.play("idle")
+			else:
+				animated_sprite.play("run")		
+		
 		# move and slow down when not moving
 		if direction:
-			velocity.x = direction * SPEED
+			if is_attacking:
+				velocity.x = direction * SPEED * ATTACK_BOOST
+			else:
+				velocity.x = direction * SPEED
+		#elif is_attacking:
+		#	velocity.x = last_dir * SPEED * ATTACK_BOOST
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 
 		move_and_slide()
+
+
+func _on_attack_timer_timeout():
+	is_attacking = false
+	attack_cooldown_timer.start()
+
+func _on_attack_cooldown_timer_timeout():
+	can_attack = true
