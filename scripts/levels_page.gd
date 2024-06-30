d1d8cdcebf60246b4c4de7cd1966c718f6b72552
Bwgga <bwgga.gi@gmail.com>
2024-06-30 19:50:09 -0400

    made lvl 1 scene
    linked button from levels page, made basic scene with palyer

--- scripts/levels_page.gd
+++ scripts/levels_page.gd
@@ -3,3 +3,7 @@ extends Node2D
 # demo level button
 func _on_demo_level_pressed():
 	get_tree().change_scene_to_file("res://scenes/demo_lvl.tscn")
+
+# level 1 button
+func _on_level_1_pressed():
+	get_tree().change_scene_to_file("res://scenes/lvl_1.tscn")
