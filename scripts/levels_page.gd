b4cbe2a37471ff97deee0e989a927b583f5ca0e4
Bwgga <bwgga.gi@gmail.com>
2024-07-10 16:40:17 -0400

    made buttons controllable via keys (UI control)

--- scripts/levels_page.gd
+++ scripts/levels_page.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+@onready var demo_level = $"Level Buttons/Demo Level"
+
+# put focus on demo level button
+func _ready():
+	demo_level.grab_focus()
+
 # demo level button
 func _on_demo_level_pressed():
 	get_tree().change_scene_to_file("res://scenes/demo_lvl.tscn")
